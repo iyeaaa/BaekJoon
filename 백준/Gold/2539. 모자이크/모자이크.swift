25aacbb8e7b0a9f7ecca89b299c6940b05763213
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:10:38 +0900

    [Gold III] Title: 모자이크, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/2539. 모자이크/모자이크.swift
@@ -0,0 +1,86 @@
+
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+let io = IO()
+let n = io.readInt(), m = io.readInt(), k = io.readInt(), l = io.readInt()
+var p = [Int]()
+var lo=0, hi=1_000_001
+
+for _ in 0..<l {
+    lo = max(lo, io.readInt()-1)
+    p.append(io.readInt())
+}
+p.sort()
+
+while lo+1 < hi {
+    let mid = (lo+hi) / 2
+    var cover = p.first!-1
+    var cnt = 0
+    p.forEach { if cover < $0 { cover=$0+mid-1; cnt+=1 } }
+    if cnt <= k {hi=mid} else {lo=mid}
+}
+
+print(hi)
+
