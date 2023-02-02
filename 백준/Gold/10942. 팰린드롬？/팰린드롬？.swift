cd05bbe0e3e2c7e568daff03da9973e530625141
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:42:38 +0900

    [Gold IV] Title: 팰린드롬?, Time: 408 ms, Memory: 97340 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.swift
@@ -0,0 +1,89 @@
+
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+let io = IO()
+let N = io.readInt()
+var L = [Int](); for _ in 0..<N {L.append(io.readInt())}
+let M = io.readInt()
+var ans = ""
+
+var dp = [[Bool]](repeating: [Bool](repeating: true, count: N), count: N)
+
+for i in 1..<N {
+    dp[i-1][i] = L[i-1] == L[i]
+}
+
+for len in 3...N {
+    for hi in len-1..<N {
+        let lo = hi-len+1
+        dp[lo][hi] = dp[lo+1][hi-1] && L[lo]==L[hi]
+    }
+}
+
+for _ in 0..<M {
+    ans += "\(dp[io.readInt()-1][io.readInt()-1] ? 1 : 0)\n"
+}
+
+print(ans)
