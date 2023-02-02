8cbea149dc9d4e93191cf9b4d7284f3ee0d90483
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:42:32 +0900

    [Bronze V] Title: ??!, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+
+
+
+public class Main {
+
+
+	
+
+
+	public static void main(String [] args) throws IOException {
+
+
+	
+
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+		StringBuilder sb = new StringBuilder(br.readLine());
+
+
+		System.out.println(sb.append("??!"));
+
+
+		
+
+
+	}
+
+
+	
+
+
+}
