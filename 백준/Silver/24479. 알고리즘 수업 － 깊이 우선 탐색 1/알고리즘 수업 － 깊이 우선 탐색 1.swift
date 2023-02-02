1a5887ae770a429d1c433987bde433a3b0b133bc
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 21:20:43 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 396 ms, Memory: 85612 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.swift
@@ -0,0 +1,34 @@
+let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+let (n, m, r) = (input[0], input[1], input[2])
+var graph = Array(repeating: [Int](), count: n+1)
+var list = Array(repeating: 0, count: n+1)
+var count = 1
+var result = ""
+
+for _ in 0..<m {
+    let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+    graph[input[0]].append(input[1])
+    graph[input[1]].append(input[0])
+}
+
+for i in 1...n {
+    graph[i].sort() {$0 < $1}
+}
+
+dfs(x: r)
+
+for i in 1...n {
+    result += "\(list[i])\n"
+}
+
+print(result)
+
+func dfs(x: Int) {
+    list[x] = count
+    count += 1
+    for i in graph[x] {
+        if list[i] == 0 {
+            dfs(x: i)
+        }
+    }
+}
