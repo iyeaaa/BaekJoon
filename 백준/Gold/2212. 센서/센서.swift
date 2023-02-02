26b00be134556f086aa345187e1bd9b1ee51e06a
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:01:01 +0900

    [Gold V] Title: 센서, Time: 16 ms, Memory: 79964 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+
+let io = IO()
+let (N, K) = (io.readInt(), io.readInt())
+let sensors = crtSensors()
+let pointGap = crtPointGap()
+// 1 3 6 6 7 9
+print(N <= K ? 0 : sensors[N-1]-sensors[0]-pointGap[..<(K-1)].reduce(0, +))
+
+
+func crtPointGap() -> [Int] {
+    var result = [Int]()
+    for i in 1..<N {
+        result.append(sensors[i]-sensors[i-1])
+    }
+    return result.sorted(by: >)
+}
+
+func crtSensors() -> [Int] {
+    var result = [Int]()
+    for _ in 0..<N {
+        result.append(io.readInt())
+    }
+    return result.sorted()
+}
