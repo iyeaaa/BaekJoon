18f11074156863e2b94d0a2bdaf7f3e43e1770f8
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 19:53:53 +0900

    [Bronze I] Title: 최소공배수, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.swift
@@ -0,0 +1,11 @@
+
+var result = ""
+for _ in 0..<Int(readLine()!)! {
+    let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+    result += "\(input[0]*input[1]/gcd(input[0], input[1]))\n"
+}
+print(result)
+
+func gcd(_ x: Int, _ y: Int) -> Int {
+    y == 0 ? x : gcd(y, x%y)
+}
