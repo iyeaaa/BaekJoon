f2d7ef13e5c0e1cb5004010ae170ce166c7b46ff
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:43:38 +0900

    [Bronze V] Title: A×B, Time: 80 ms, Memory: 11428 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+
+
+public class Main {
+
+
+	public static void main(String [] args) throws IOException {
+
+
+		int a = System.in.read()-48;
+
+
+		System.in.read();
+
+
+		int b = System.in.read()-48;
+
+
+		System.out.println( a*b );
+
+
+	}
+
+
+}
