d192888bb0a1e17d22ecbf28d6a586006df5fa17
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:33:32 +0900

    [Gold IV] Title: 사냥꾼, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.swift
@@ -0,0 +1,90 @@
+
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+let io = IO()
+let M = io.readInt(), N = io.readInt(), L = io.readInt()
+var hunter = [Int](); for _ in 0..<M {hunter.append(io.readInt())}; hunter.sort()
+var animal = [(x: Int, y: Int)](), aidx = 0, hidx = 0
+var ans = 0
+
+for _ in 0..<N {
+    animal.append((io.readInt(), io.readInt()))
+}
+animal.sort{$0.x < $1.x}
+
+while hidx < M && aidx < N {
+    let (x, y) = animal[aidx]
+    let hx = hunter[hidx]
+    if x < hx {
+        if L - (hx-x) >= y {ans += 1}
+        aidx += 1
+    } else if L - (x-hx) >= y {
+        ans += 1; aidx += 1
+    } else {
+        hidx += 1
+    }
+}
+
+print(ans)
