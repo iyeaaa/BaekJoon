76adff34071346dae107b18d21fe5e0a82fc43f6
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 21:01:35 +0900

    [Silver III] Title: N과 M (7), Time: 432 ms, Memory: 132092 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.swift
@@ -0,0 +1,17 @@
+let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+let (n, m) = (input[0], input[1])
+let seq = readLine()!.split{$0==" "}.map{Int(String($0))!}.sorted()
+var result = ""
+
+bkTrk(0, "");
+print(result)
+
+func bkTrk(_ digit: Int,_ strn: String) {
+    if digit == m {
+        result += strn + "\n"
+        return
+    }
+    for i in seq {
+        bkTrk(digit+1, strn + "\(i) ")
+    }
+}
