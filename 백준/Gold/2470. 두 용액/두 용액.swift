630a83c5923fc6f4e2acc666c81aafb686cbd02e
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:08:12 +0900

    [Gold V] Title: 두 용액, Time: 28 ms, Memory: 83628 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+let io = IO()
+let N = io.readInt()
+var liquids = [Int](); for _ in 0..<N { liquids.append(io.readInt()) }; liquids.sort()
+var (lf, ryt) = (0, N-1)
+var prior_min = Int.max
+var (val1, val2) = (0, 0)
+
+while lf < ryt {
+    let sum = liquids[lf] + liquids[ryt]
+    if prior_min > abs(sum) {
+        prior_min = abs(sum)
+        val1 = liquids[lf]
+        val2 = liquids[ryt]
+    }
+    if sum <= 0 {
+        lf += 1
+    } else {
+        ryt -= 1
+    }
+}
+
+print(val1, val2)
+
+
