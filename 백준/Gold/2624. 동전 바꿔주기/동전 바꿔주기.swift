271630071135538a4239c0828e6b1f08f0afbb66
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:13:31 +0900

    [Gold V] Title: 동전 바꿔주기, Time: 44 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.swift
@@ -0,0 +1,89 @@
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+
+let io = IO()
+let (T, k) = (io.readInt(), io.readInt())
+typealias Coin = (p: Int, n: Int)
+let coins: [Coin] = crtCoins()
+var dp = [Int](repeating: 0, count: T+1); dp[0] = 1
+
+for (price, n) in coins {
+    for i in stride(from: T, through: price, by: -1) {
+        for cnt in 1...n where price*cnt <= i {
+            dp[i] += dp[i-price*cnt]
+        }
+    }
+}
+
+print(dp[T])
+
+func crtCoins() -> [Coin] {
+    var result = [Coin]()
+    for _ in 0..<k {
+        result.append(Coin(p: io.readInt(), n: io.readInt()))
+    }
+    return result
+}
+
