07b8521c5a1bcdd25200da7b7ae264aa2aec0619
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:27:43 +0900

    [Bronze IV] Title: 상근날드, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.swift
@@ -0,0 +1,8 @@
+var b = Int.max
+var d = Int.max
+b = min(b, Int(readLine()!)!)
+b = min(b, Int(readLine()!)!)
+b = min(b, Int(readLine()!)!)
+d = min(d, Int(readLine()!)!)
+d = min(d, Int(readLine()!)!)
+print(b+d-50)
