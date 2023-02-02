9c56de8ddcf1b9df5b903750d3f1f9399e1ca7c4
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 21:10:19 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 84 ms, Memory: 88820 KB -BaekjoonHub

---
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.swift
@@ -0,0 +1,98 @@
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+struct UnionFind {
+    var parent: [Int]
+
+    init(cnt: Int) { parent = Array(0...cnt) }
+
+    mutating func root(of x: Int) -> Int {
+        if parent[x] == x { return x }
+        parent[x] = root(of: parent[x])
+        return parent[x]
+    }
+
+    mutating func merge(_ x: Int, with y: Int) {
+        let (x, y) = (root(of: x), root(of: y))
+        if x == y { return }
+        parent[x] = y
+    }
+
+    mutating func isUnion(_ x: Int, _ y: Int) -> Bool {
+        root(of: x) == root(of: y)
+    }
+}
+
+let io = IO()
+let N = io.readInt()
+var uf = UnionFind(cnt: N)
+for _ in 0..<(N-2) {
+    uf.merge(io.readInt(), with: io.readInt())
+}
+
+for i in 2...N {
+    if !uf.isUnion(1, i) {
+        print(1, i); exit(0)
+    }
+}
