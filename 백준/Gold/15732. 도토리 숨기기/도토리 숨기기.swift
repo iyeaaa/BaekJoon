3413bef7e3b0a8d64e247cba2888a7c9c9554159
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 21:03:06 +0900

    [Gold II] Title: 도토리 숨기기, Time: 16 ms, Memory: 80496 KB -BaekjoonHub

---
+++ 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.swift
@@ -0,0 +1,91 @@
+
+import Foundation
+
+final class IO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                      || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+
+    @inline(__always) func writeByString(_ output: String) { // wapas
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+let io = IO()
+var (n, k, d) = (io.readInt(), io.readInt(), io.readInt())
+var rule = [(a:Int,b:Int,c:Int)]()
+
+for _ in 0..<k {
+    rule.append((io.readInt(), io.readInt(), io.readInt()))
+}
+
+var lo=0, hi=n+1
+while lo + 1 < hi {
+    let mid = (lo + hi) / 2
+    if !psb(mid) {lo=mid} else {hi=mid}
+}
+print(hi)
+
+func psb(_ x: Int) -> Bool {
+    var cnt = 0
+    for (a, b, c) in rule {
+        if b <= x {
+            cnt += (b-a)/c + 1
+        } else if a <= x {
+            cnt += (x-a)/c + 1
+        }
+    }
+    return cnt >= d
+}
