80975b7b7c929ff4146560c9c1661ac71bee2ddf
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 19:46:50 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1880 ms, Memory: 130932 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.swift
@@ -0,0 +1,46 @@
+struct Distjoint_Set {
+    private var parent: [Int]
+    private var rank: [Int]
+
+    init(_ x: Int) {
+        parent = Array(0..<x)
+        rank = [Int](repeating: 0, count: x)
+    }
+
+    mutating func root(_ x: Int) -> Int {
+        if parent[x] == x { return x }
+        parent[x] = root(parent[x])
+        return parent[x]
+    }
+
+    mutating func uni(_ x: Int, _ y: Int) {
+        let x = root(x), y = root(y)
+        if rank[x] < rank[y] { parent[x] = y; }
+        else { parent[y] = x; }
+        if rank[x] == rank[y] { rank[x] += 1 }
+    }
+
+    mutating func isUni(_ x: Int, _ y: Int) -> Bool {
+        root(x) == root(y)
+    }
+}
+
+let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+let n = input[0], m = input[1]
+var e = [(Int, Int, Int)]()
+var uf = Distjoint_Set(n+1)
+var mv = 0
+var ans = 0
+
+for _ in 0..<m {
+    let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+    e.append((input[0], input[1], input[2]))
+}
+
+
+for (a, b, cost) in e.sorted(by: {$0.2 < $1.2}) where !uf.isUni(a, b) {
+    ans += cost; uf.uni(a, b)
+    mv = max(mv, cost)
+}
+
+print(ans - mv)
