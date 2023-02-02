301d18fd71291a3a83efc865f719e1bcff7b3128
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:29:55 +0900

    [Gold IV] Title: 문자열 생성, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Gold/6137. 문자열 생성/문자열 생성.swift
@@ -0,0 +1,1 @@
+let N=Int(readLine()!)!;var S=[Character](),ans="";for _ in 0..<N {S.append(Character(readLine()!))};for i in 1...N {var lo=0,hi=S.count-1;while lo<=hi&&S[lo]==S[hi]{lo+=1;hi-=1};ans.append(lo>hi ? S.popLast()!:S[lo]<S[hi] ? S.removeFirst():S.popLast()!);if i%80==0{ans+="\n"}};print(ans)
