4d90188364c369a3eadbe5722630f875a4544c96
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-02 20:48:24 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 228 ms, Memory: 80928 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -0,0 +1,21 @@
+let n = Int(readLine()!)!
+var array = [[Int]]()
+var result = ""
+
+for _ in 0..<n {
+    let point = readLine()!.split{$0==" "}.map{Int(String($0))!}
+    array.append(point)
+}
+
+array.sort(by: {
+    if $0[0] == $1[0] {
+        return $0[1] < $1[1]
+    }
+    return $0[0] < $1[0]
+})
+
+for i in 0..<n {
+    result += "\(array[i][0]) \(array[i][1]) \n"
+}
+
+print(result)
