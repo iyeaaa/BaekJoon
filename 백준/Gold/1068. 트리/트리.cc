059e826d7c8c7291a1cd0c869c4c92e9af1182be
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-16 18:32:01 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, rmv, root;
+int ans;
+vector<int> g[51];
+
+void dfs(int node) {
+    if (node == rmv)
+        return;
+    if (g[node].empty()) {
+        ans++;
+        return;
+    }
+    for (auto child: g[node])
+        dfs(child);
+}
+
+int main() {
+    cin >> N;
+    for (int i=0; i<N; i++) {
+        int p; cin >> p;
+        if (p != -1)
+            g[p].push_back(i);
+        else
+            root = i;
+    }
+    cin >> rmv;
+    
+    for (int i=0; i<N; i++) {
+        auto idx = find(g[i].begin(), g[i].end(), rmv);
+        if (idx != g[i].end())
+            g[i].erase(idx);
+    }
+    
+    dfs(root);
+    
+    cout << ans;
+}
