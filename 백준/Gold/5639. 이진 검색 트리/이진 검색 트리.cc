c6a98eecaf5b40474901267cb289d3aa6591f027
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-02-16 23:16:30 +0900

    [Gold V] Title: 이진 검색 트리, Time: 0 ms, Memory: 2560 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -1,35 +1,21 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
-int t;
-vector<int> tree;
+int tree[10001];
 
-// lo is Root
 void dfs(int lo, int hi) {
-    if (lo > hi)
-        return;
-    if (lo == hi) {
-        cout << tree[lo] << '\n';
-        return;
-    }
-    int flag = false;
-    for (int i=lo+1; i<=hi; i++) {
-        if (tree[lo] < tree[i]) {
-            dfs(lo+1, i-1);
-            dfs(i, hi);
-            flag = true;
-            break;
-        }
-    }
-    if (!flag)
-        dfs(lo+1, hi);
+    if (lo >= hi) return;
+    auto it = upper_bound(tree+lo, tree+hi, tree[lo]);
+    int mid = distance(tree, it);
+    dfs(lo+1, mid);
+    dfs(mid, hi);
     cout << tree[lo] << '\n';
 }
 
 int main() {
-    while (cin >> t)
-        tree.push_back(t);
-
-    dfs(0, tree.size()-1);
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int i=0;
+    while (cin>>tree[i++]);
+    dfs(0, i-1);
 }
