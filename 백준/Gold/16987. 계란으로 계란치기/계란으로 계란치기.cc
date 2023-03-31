fe4da7c0516ce79a6d0df1d8a026f23d358edc40
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-03-31 10:08:34 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define F(i, l, r) for (int i=l; i<r; i++)
+using namespace std;
+
+int n, ans;
+pair<int, int> eggs[8];
+
+bool allBroken(int idx) {
+    F(i, 0, n) if (idx != i && eggs[i].first > 0) return false;
+    return true;
+}
+
+void f(int idx, int sum) {
+    ans = max(ans, sum);
+
+    if (idx >= n) {
+        return;
+    }
+
+    if (eggs[idx].first <= 0 || allBroken(idx)) {
+        f(idx + 1, sum);
+        return;
+    }
+
+    F(i, 0, n) if (idx != i && eggs[i].first > 0) {
+        auto& [s1, w1] = eggs[idx];
+        auto& [s2, w2] = eggs[i];
+
+        s1 -= w2;
+        s2 -= w1;
+
+        f(idx+1, sum + (s1 <= 0) + (s2 <= 0));
+
+        s1 += w2;
+        s2 += w1;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+    cin >> n;
+    F(i, 0, n) cin >> eggs[i].first >> eggs[i].second;
+    f(0, 0);
+
+    cout << ans;
+}
