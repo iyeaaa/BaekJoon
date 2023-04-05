f80dcd3dec977b82dd857e1bde928135edf59c81
iyeaaa <102817453+iyeaaa@users.noreply.github.com>
2023-04-05 14:05:20 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 104 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.cc
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.cc
@@ -4,9 +4,7 @@
 #define all(x) (x).begin(), (x).end()
 using namespace std;
 
-int n, w, vst[500'001], leafcnt;
-vector<int> g[500'001];
-queue<int> q;
+int n, w, a[500'001], leafcnt;
 
 int main() {
     ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
@@ -15,24 +13,9 @@ int main() {
     F(i, 0, n-1) {
         int u, v;
         cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        a[u]++, a[v]++;
     }
-
-    q.push(1);
-    vst[1] = true;
-
-    while (!q.empty()) {
-        int cur = q.front();
-        q.pop();
-        bool exist = false;
-        for (int nxt: g[cur]) if (!vst[nxt]) {
-            exist = true;
-            q.push(nxt);
-            vst[nxt] = true;
-        }
-        leafcnt += !exist;
-    }
+    F(i, 2, n+1) leafcnt += a[i] == 1;
 
     cout.precision(4);
     cout << fixed << (1.0 * w / leafcnt) << endl;
