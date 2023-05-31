07c36bc811a866675ccca9dd8a3ed22cf1af9d3d
yein Yi <iyeaaa@o.cnu.ac.kr>
2023-05-31 11:17:55 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 32 ms, Memory: 7076 KB -BaekjoonHub

---
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define FIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define FOR(i, l, r) for (int i = l; i < r; i++)
+using namespace std;
+typedef pair<int, int> pii;
+
+int n, m, a, b, c, s, t, mndist[5001];
+vector<pii> g[100'001];
+priority_queue<pii, vector<pii>, greater<>> pq;
+
+int main() {
+    FIO
+    cin >> n >> m;
+    FOR(i, 0, m)
+        cin >> a >> b >> c,
+        g[a].emplace_back(c, b),
+        g[b].emplace_back(c, a);
+    cin >> s >> t;
+    fill(mndist, mndist+5000, 99999999);
+
+    pq.emplace(0, s);
+    while (!pq.empty()) {
+        auto [edge, cur] = pq.top();
+        pq.pop();
+        if (edge > mndist[cur]) continue;
+        for (auto [dist, nxt]: g[cur]) {
+            if (edge + dist >= mndist[nxt]) continue;
+            mndist[nxt] = edge + dist;
+            pq.emplace(edge + dist, nxt);
+        }
+    }
+
+    cout << mndist[t];
+}
